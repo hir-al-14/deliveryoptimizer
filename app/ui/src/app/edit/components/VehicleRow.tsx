4ce97e8c1c969a2e711bec3cb043b3762c5941d3
Guritfak Gill <guritfak2007@gmail.com>
2026-04-01 20:10:59 -0700

    fix: change desktop to display cubic feet instead of volume

--- app/ui/src/app/edit/components/VehicleRow.tsx
+++ app/ui/src/app/edit/components/VehicleRow.tsx
@@ -458,7 +458,7 @@ export default function VehicleRow({
         <option value="units">Units</option>
         <option value="lbs">Lbs</option>
         <option value="kgs">Kgs</option>
-        <option value="volume">Volume</option>
+        <option value="cubic_feet">Cubic Feet</option>
       </select>
       <input
         type="number"
